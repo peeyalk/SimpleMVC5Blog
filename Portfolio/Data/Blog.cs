309d9cccbd639c7599fb36b9505ef8232e0bcbc7
Long Nguyen <conchutgidenho2290@yahoo.com>
2015-04-23 00:46:33 -0700

    Initial commit

---
+++ Portfolio/Data/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.Data
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Created { get; set; }
+
+    }
+}
---
+++ Portfolio/Data/BlogRepository.cs
@@ -0,0 +1,29 @@
+﻿using Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.Data
+{
+    public class BlogRepository : IBlogRepository
+    {
+
+        private ApplicationDbContext _BlogContext;
+
+        public BlogRepository(ApplicationDbContext BlogContext)
+        {
+            _BlogContext = BlogContext;
+        }
+
+        public IQueryable<Blog> getBlogs()
+        {
+            return _BlogContext.Blogs;
+        }
+
+        public void Save()
+        {
+            _BlogContext.SaveChanges();
+        }
+    }
+}
