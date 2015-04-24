64d70081abc39edf895bcb3fa91f694ef949d2ea
ln206 <lnguye81@uw.edu>
2015-04-23 17:31:09 -0700

    Added docs to Home Controller

--- Portfolio/Controllers/HomeController.cs
+++ Portfolio/Controllers/HomeController.cs
@@ -8,11 +8,19 @@ namespace Portfolio.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Home page
+        /// </summary>
+        /// <returns>index view</returns>
         public ActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Contact page
+        /// </summary>
+        /// <returns>Contact view</returns>
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
